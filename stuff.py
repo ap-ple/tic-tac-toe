22d6adf539da8949d1422d8303339bab8f473333
ap-ple <55882424+ap-ple@users.noreply.github.com>
2020-03-02 21:31:26 -0500

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -1,7 +1,7 @@
 import os
 
 #a shorter version of system('cls')
-def cls():
+def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
 #Like input, but only accepts numbers; returns number in integer form, or 0 if the input is not a number
@@ -12,26 +12,26 @@ def num_input(string=""):
 #Like input, but clears after the input is taken
 def dialogue(string=""):
     x = input(string)
-    cls()
+    clear()
     return x
 
 #returns a string of the simplest radical form of the square root of a number
 def radical(num):
     numlist = []
     while True: 
-      for n in range(2, 999):
-        if num % (n ** 2) == 0:
-          num /= (n ** 2)
-          numlist.append(n)
-          break
-      else:
-        break
+        for n in range(2, 999):
+            if num % (n ** 2) == 0:
+                num /= (n ** 2)
+                numlist.append(n)
+                break
+        else:
+            break
     newnum = 1
     for n in numlist:
-      newnum *= n
+        newnum *= n
     if newnum == 1:
-      return f"√{int(num)}"
+        return f"√{int(num)}"
     elif num == 1:
-      return newnum
+        return newnum
     else:
-      return f"{newnum}√{int(num)}"
+        return f"{newnum}√{int(num)}"
