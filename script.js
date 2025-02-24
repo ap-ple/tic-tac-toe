9d231d82be53f09ec46f51b57a370f8e479e980d
Johan Durish <jdurish2@gmail.com>
2025-02-24 15:01:41 -0500

    Add gameboard and basic formatting

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const gameboard = (function(root) {
+   let board = Array(9);
+   
+   let boardElement = root.querySelector("body>main>.gameboard");
+   
+   for (let i = 0; i < board.length; i++) {
+      let element = root.createElement("button");
+      board[i] = element;
+      boardElement.appendChild(element);
+   }
+
+   const resetBoard = () => board.map(element => element.innerText = "X");
+
+   resetBoard();
+
+   return {board, resetBoard};
+})(document);
