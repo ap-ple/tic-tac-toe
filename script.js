a7893964ef039b2d546d37c9c4f1bf9a06e989d5
Johan Durish <jdurish2@gmail.com>
2025-02-25 09:59:29 -0500

    Add basic UI interaction

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 
-const gameboard = (function(root) {
+const mainGameboard = (function(root) {
    let board = Array(9);
    
    let boardElement = root.querySelector("body>main>.gameboard");
    
    for (let i = 0; i < board.length; i++) {
-      let element = root.createElement("button");
-      board[i] = element;
-      boardElement.appendChild(element);
+      let slotElement = root.createElement("button");
+      board[i] = slotElement;
+      boardElement.appendChild(slotElement);
    }
 
-   const resetBoard = () => board.map(element => element.innerText = "");
+   const resetBoard = () => board.map(element => {
+      element.innerText = ""
+      element.disabled = false;
+   });
 
    resetBoard();
 
-   return {resetBoard};
+   return {board, resetBoard};
 })(document);
+
+const mainGame = (function(root, gameboard) {
+   const symbols = ["O", "X"]
+   let turn = 1;
+
+   const play = (slotElement) => {
+      if (slotElement.innerText.length === 0) {
+         slotElement.innerText = symbols[turn++ % symbols.length];
+         slotElement.disabled = true;
+      }
+   };
+
+   gameboard.board.forEach(element => {
+      element.addEventListener("click", event => play(event.target));
+   });
+
+   return {};
+})(document, mainGameboard);
