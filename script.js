5c7a4353ffec113fe33ed83c8c460e423bc1ce41
Johan Durish <jdurish2@gmail.com>
2025-02-25 11:06:08 -0500

    Add const declarations

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 const mainGameboard = (function(root) {
-   let board = Array(9);
+   const board = Array(9);
    
-   let boardElement = root.querySelector("body>main>.gameboard");
+   const boardElement = root.querySelector("body>main>.gameboard");
    
    for (let i = 0; i < board.length; i++) {
-      let spaceElement = root.createElement("button");
+      const spaceElement = root.createElement("button");
       board[i] = spaceElement;
       boardElement.appendChild(spaceElement);
    }
