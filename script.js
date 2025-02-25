a8f2e177b1095e7880f26794cd79143ed47301fe
Johan Durish <jdurish2@gmail.com>
2025-02-25 10:11:48 -0500

    Add turn message

--- script.js
+++ script.js
@@ -24,11 +24,15 @@ const mainGame = (function(root, gameboard) {
    const symbols = ["X", "O"]
    let turn = 1;
 
+   const messageElement = root.querySelector("body>main>.message");
+
    const play = (slotElement) => {
       if (slotElement.innerText.length === 0) {
          slotElement.innerText = symbols[(turn - 1) % symbols.length];
          slotElement.disabled = true;
          turn++;
+
+         messageElement.innerText = `Your turn, Player ${symbols[(turn - 1) % symbols.length]}.`;
       }
    };
 
